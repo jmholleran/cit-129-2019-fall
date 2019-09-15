566ae4393e02e52bb0af6ac07dd1acea26e2d7d7
Joe Holleran <joeyholleran@gmail.com>
2019-09-15 14:30:26 -0400

    Week 2 Loops - Finished

---
+++ mini_program_dictionary.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Joe Holleran
+Python 2
+Mini-Program: Dictionary
+09/11/2019
+"""
+
+name = {'Peter', 'Paul', 'Mary'}
+customer_id = {123, 456, 789}
+address = {'1 Main St', '2 Maple Ave', '3 5th Avenue'}
+
+def main():
+    
+    print()
+    
+    display_dictionary()        
+    mod_dictionary()
+    add_dictionary()
+    
+def display_dictionary():  
+    
+    
+def mod_dictionary():
+       
+    print()
+    
+    
+    
+def add_dictionary():
+    
+    print()
+    
+if __name__ == "__main__":
+    main()
+
+
