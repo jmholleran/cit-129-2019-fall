f0851079cbf10dc5cb9c0d7da8e98d5e84eff93c
Joe Holleran <joeyholleran@gmail.com>
2019-10-02 17:47:15 -0400

    test

--- 311_program.py
+++ 311_program.py
@@ -9,10 +9,7 @@ import json
 
 
 with open('wprdc_json.txt', 'r') as fields:
-    wprdc_json = json.load(fields)
+    wprdc = json.load(fields)
     
-for word in wprdc_json:
-    print(word, end="")
-
     
 
